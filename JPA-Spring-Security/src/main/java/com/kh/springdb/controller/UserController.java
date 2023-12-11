ed276b1fa745590231b314c263a8dd4813fba2ec
Kaye <139112573+2Mihye@users.noreply.github.com>
2023-12-11 18:20:53 +0900

    Spring Security

--- JPA-Spring-Security/src/main/java/com/kh/springdb/controller/UserController.java
+++ JPA-Spring-Security/src/main/java/com/kh/springdb/controller/UserController.java
@@ -3,6 +3,7 @@ package com.kh.springdb.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.kh.springdb.model.UserCheckForm;
@@ -20,10 +21,11 @@ public class UserController {
 	// 회원가입할 때 들어갈 창 조회
 	@GetMapping("/signup")
 	public String signUP(UserCheckForm userCheckForm) {
-		return "signup-form";
+		return "signup_form";
 	}
 	
 	// 회원가입을 진행할 때 값이 존재하는지 원하는 조건에 맞는지 확인 후 전달
+	@PostMapping("/signup")
 	public String signUp(@Valid UserCheckForm usercheckform, BindingResult bindingResult) {
 		// 1. 비밀번호와 비밀번호 확인이 일치하지 않을 때
 		/* bindingResult.rejectValue(a, b, c) : 
